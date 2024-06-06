dcb15feaac6f5d7bca290866dc3dea0a7415c217
Victor Golob <victorgolob@gmail.com>
2024-06-06 15:12:01 -0300

    add rule to error at non exist module

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'eslint-config-prettier',
+    'plugin:import/errors',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -18,5 +19,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-unresolved': 'error',
   },
 };
