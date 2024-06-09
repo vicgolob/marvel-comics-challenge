5451105803ec101f82372e1a27a14b3e5af666e0
Victor Golob <victorgolob@gmail.com>
2024-06-08 22:53:47 -0300

    fix broken App UT

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('App', () => {
-  // renders without crashing
   it('should render without crashing', () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(container).toBeInTheDocument();
-  });
-
-  // renders "App is running!" text
-  it('should render the correct heading text', () => {
-    const { getByText } = render(<App />);
-    const headingElement = getByText('App is running!');
-    expect(headingElement).toBeInTheDocument();
   });
 });
