a024e95b7c92ef67b3ac9a4a44f148142786b0aa
Victor Golob <victorgolob@gmail.com>
2024-06-08 22:51:59 -0300

    add components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as Favorites } from './Favorites/Favorites.jsx';
+export { default as Header } from './Header/Header.jsx';
+export { default as CharacterCard } from './CharacterCard/CharacterCard.jsx';
+export { default as ProgressBar } from './ProgressBar/ProgressBar.jsx';
