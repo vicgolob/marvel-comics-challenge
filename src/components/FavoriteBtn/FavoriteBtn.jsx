2d7325bf45a74c393b2d0f49ff67d1d447665be2
Victor Golob <victorgolob@gmail.com>
2024-06-12 11:00:42 -0300

    favoriteBtn - add custom size prop

--- src/components/FavoriteBtn/FavoriteBtn.jsx
+++ src/components/FavoriteBtn/FavoriteBtn.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import IconHeartRed from '@/assets/icon-heart-red.svg?react';
 import IconHeartWhite from '@/assets/icon-heart-white.svg?react';
 
-function FavoriteBtn({ isFavorite = false, handleClick }) {
+function FavoriteBtn({ isFavorite = false, handleClick, isLarge = false }) {
   return (
     <button className="icon-btn" onClick={handleClick}>
       {isFavorite ? (
@@ -11,10 +11,13 @@ function FavoriteBtn({ isFavorite = false, handleClick }) {
           onClick={() => {}}
           data-testid="red-heart-icon"
           className="is-favorite-icon"
-          width="12px"
+          width={isLarge ? '24px' : '12px'}
         />
       ) : (
-        <IconHeartWhite data-testid="red-heart-empty" width="12px" />
+        <IconHeartWhite
+          data-testid="red-heart-empty"
+          width={isLarge ? '24px' : '12px'}
+        />
       )}
     </button>
   );
@@ -25,4 +28,5 @@ export default FavoriteBtn;
 FavoriteBtn.propTypes = {
   isFavorite: PropTypes.bool,
   handleClick: PropTypes.func,
+  isLarge: PropTypes.bool,
 };
